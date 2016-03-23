b1dfd9c8bfef41bf7ee97d7528b1923c09748455
ggdaltoso <gabriel.daltoso@opus-software.com.br>
2016-03-23 17:44:53 -0300

    working with sharejs and redis (memcached)

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express');
+
+// get sharejs dependencies
+var sharejs = require('share');
+var redisClient = require('redis').createClient();
+
 var app = express();
 
 // set the view engine to ejs
@@ -11,8 +16,17 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('pad');
 });
+app.get('/(:id)', function(req, res) {
+  res.render('pad');
+});
+
+// options for sharejs
+var options = {
+  db: {type: 'redis', client: redisClient}
+};
+
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost)
 var port = process.env.PORT || 8000;
 app.listen(port);
-
